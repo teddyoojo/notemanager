dcce8164fe7e74b6d8b4063a009cf1e6e1af03f0
Philipp Brendel <philipp.brendel@ruhr-uni-bochum.de>
2022-02-28 16:40:51 +0100

    Initial commit

---
+++ src/test/java/brendel/philipp/notemanager/NotemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package brendel.philipp.notemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
