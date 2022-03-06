dd148388443fe5cb978e12834ef96014ea1ed1d5
Philipp Brendel <philipp.brendel@ruhr-uni-bochum.de>
2022-03-06 22:00:46 +0100

    changed some styling
    added margins to submit button, put notebook.component into body

--- frontend/notemanagerapp/src/app/app.component.ts
+++ frontend/notemanagerapp/src/app/app.component.ts
@@ -2,13 +2,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title(title: any) {
-      throw new Error('Method not implemented.');
+    throw new Error('Method not implemented.');
   }
   constructor() {}
   ngOnInit() {}
 }
-
